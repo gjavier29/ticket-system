7378f7800a452a8c12f51561b2803a0dc0cab1a4
gjavier <gjavier@csyoutsourcing.com>
2022-12-21 14:59:28 +0800

    replace app with typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      sample
-      {/* <header className="App-header">
+      <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
           className="App-link"
@@ -18,7 +18,7 @@ function App() {
         >
           Learn React
         </a>
-      </header> */}
+      </header>
     </div>
   );
 }
