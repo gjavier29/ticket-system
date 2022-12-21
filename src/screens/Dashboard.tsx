7dd41e498f6515b632b489f6e51a4590f8fa6740
gjavier <gjavier@csyoutsourcing.com>
2022-12-21 15:50:52 +0800

    update dashboard with css

--- src/screens/Dashboard.tsx
+++ src/screens/Dashboard.tsx
@@ -1,12 +1,81 @@
-import React from 'react'
+import React, { useState } from "react";
+import {
+  MenuFoldOutlined,
+  MenuUnfoldOutlined,
+  UploadOutlined,
+  UserOutlined,
+  VideoCameraOutlined,
+} from "@ant-design/icons";
+import { Layout, Menu, theme } from "antd";
+import './Dashboard.css'
 
-type Props = {}
 
-const Dashboard = (props: Props) => {
+const { Header, Sider, Content } = Layout;
+type Props = {};
+
+const Dashboard: React.FC = (props: Props) => {
+  const [collapsed, setCollapsed] = useState(false);
+  const {
+    token: { colorBgContainer },
+  } = theme.useToken();
   return (
-    <div>Dashboard</div>
-  )
-}
+    <Layout className="height-100">
+      <Sider trigger={null} collapsible collapsed={collapsed}>
+        <div className="logo" />
+        <Menu
+          theme="dark"
+          mode="inline"
+          defaultSelectedKeys={["1"]}
+          items={[
+            {
+              key: "1",
+              icon: <UserOutlined />,
+              label: "nav 1",
+            },
+            {
+              key: "2",
+              icon: <VideoCameraOutlined />,
+              label: "nav 2",
+            },
+            {
+              key: "3",
+              icon: <UploadOutlined />,
+              label: "nav 3",
+            },
+            {
+              key: "4",
+              icon: <UploadOutlined />,
+              label: "nav 4",
+            },
+          ]}
+        />
+      </Sider>
 
-export default Dashboard
+      <Layout className="site-layout">
+        <Header style={{ padding: 0, background: colorBgContainer }}>
+          <div className="">
+            {React.createElement(
+              collapsed ? MenuUnfoldOutlined : MenuFoldOutlined,
+              {
+                className: "trigger",
+                onClick: () => setCollapsed(!collapsed),
+              }
+            )}
+          </div>
+        </Header>
+        <Content
+          style={{
+            margin: "24px 16px",
+            padding: 24,
+            minHeight: 280,
+            background: colorBgContainer,
+          }}
+        >
+          Content
+        </Content>
+      </Layout>
+    </Layout>
+  );
+};
 
+export default Dashboard;
