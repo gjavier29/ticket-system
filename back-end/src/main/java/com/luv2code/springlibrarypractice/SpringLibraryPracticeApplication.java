d218b120c5c1621304dd83e080d37fdaed4853f9
gjavier <gjavier@csyoutsourcing.com>
2022-12-23 10:57:28 +0800

    added spring-boot backend

---
+++ back-end/src/main/java/com/luv2code/springlibrarypractice/SpringLibraryPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.luv2code.springlibrarypractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLibraryPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringLibraryPracticeApplication.class, args);
+	}
+
+}
---
+++ back-end/src/test/java/com/luv2code/springlibrarypractice/SpringLibraryPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luv2code.springlibrarypractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLibraryPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
