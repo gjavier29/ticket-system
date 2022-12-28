d8b8d89019f3cd4f0b588f8de7ad03df3abdf7a3
javiergenepaul <javeirgenepaul@example.com>
2022-12-28 20:38:51 +0800

    update custom response

--- back-end/src/main/java/com/crudpractice/crud/controller/ProductController.java
+++ back-end/src/main/java/com/crudpractice/crud/controller/ProductController.java
@@ -2,69 +2,112 @@ package com.crudpractice.crud.controller;
 
 
 import com.crudpractice.crud.model.Product;
+import com.crudpractice.crud.response.ResponseHandler;
 import com.crudpractice.crud.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("api")
 public class ProductController {
-    @RequestMapping("/")
-    public String welcomePage() {
-        return "Welcome to Yawin Tutor";
-    }
-
     @Autowired
     private ProductService service;
 
+    @GetMapping("/hello-world")
+    public ResponseEntity<Object> HelloWorld(){
+        try {
+            List<Product> prod = service.getProducts();
+            String message = "Hello World Example";
+            return ResponseHandler.generateResponse(message, HttpStatus.OK, prod);
+        } catch (Exception e) {
+            return ResponseHandler.generateResponse(e.getMessage(), HttpStatus.MULTI_STATUS, null);
+        }
+    }
+
+
     @PostMapping("/add-product")
-    public Product addProduct(@RequestBody Product product) {
-        return service.saveProduct(product);
+    public ResponseEntity<Object> addProduct(@RequestBody Product product) {
+        try {
+            Product prod = service.saveProduct(product);
+            String message = "Successfully Added Product";
+            return ResponseHandler.generateResponse(message, HttpStatus.OK, prod);
+        } catch (Exception e) {
+            return ResponseHandler.generateResponse(e.getMessage(), HttpStatus.MULTI_STATUS, null);
+        }
     }
 
     @PostMapping("/add-products")
-    public List<Product> addProduct(@RequestBody List<Product> products) {
-        return (List<Product>) service.saveProducts(products);
-    }
-
-    @GetMapping("/hello-world/{id}")
-    public int helloWorld(@PathVariable int id) {
-//        return service.helloWorld();
-        return id;
+    public ResponseEntity<Object> addProduct(@RequestBody List<Product> products) {
+        try {
+//            return (List<Product>) service.saveProducts(products);
+            List<Product> prod = service.saveProducts(products);
+            String message = "Successfully Added Products";
+            return ResponseHandler.generateResponse(message, HttpStatus.OK, prod);
+        } catch (Exception e) {
+            return ResponseHandler.generateResponse(e.getMessage(), HttpStatus.MULTI_STATUS, null);
+        }
     }
 
     @GetMapping("/products")
-    public List<Product> findAllProducts() {
-//        List<JSONPObject> entities = new ArrayList<JSONPObject>();
-//        JSONObject entity = new JSONObject();
-        List<Product> prod = service.getProducts();
-
-        return prod;
+    public ResponseEntity<Object> findAllProducts() {
+        try {
+            List<Product> prod = service.getProducts();
+            String message = "Product List.";
+            int count =
+            return ResponseHandler.generateResponse(message, HttpStatus.OK, prod);
+        } catch (Exception e) {
+            return ResponseHandler.generateResponse(e.getMessage(), HttpStatus.MULTI_STATUS, null);
+        }
     }
 
     @GetMapping("/product/{id}")
-    public Product findProductById(@PathVariable int id) {
-        return service.getProductById(id);
+    public ResponseEntity<Object> findProductById(@PathVariable int id) {
+        try {
+            Product prod = service.getProductById(id);
+            String message = "Product Details.";
+            return ResponseHandler.generateResponse(message, HttpStatus.OK, prod);
+        } catch (Exception e) {
+            return ResponseHandler.generateResponse(e.getMessage(), HttpStatus.MULTI_STATUS, null);
+        }
     }
 
     @GetMapping("/product-name/{name}")
-    public Product findProductByName(@PathVariable String name) {
-        return service.getProductByName(name);
+    public ResponseEntity<Object> findProductByName(@PathVariable String name) {
+        try {
+            Product prod = service.getProductByName(name);
+            String message = "Product Details.";
+            return ResponseHandler.generateResponse(message, HttpStatus.OK, prod);
+        } catch (Exception e) {
+            return ResponseHandler.generateResponse(e.getMessage(), HttpStatus.MULTI_STATUS, null);
+        }
     }
 
     @PutMapping("/product/{id}")
-    public Product updateProduct(@PathVariable int id, @RequestBody Product product) {
-//        return id;
-//        return id + product;
-        return service.updateProduct(id, product);
+    public ResponseEntity<Object> updateProduct(@PathVariable int id, @RequestBody Product product) {
+        try {
+            Product prod = service.updateProduct(id, product);
+            String message = "Product Details.";
+            return ResponseHandler.generateResponse(message, HttpStatus.OK, prod);
+        } catch (Exception e) {
+            return ResponseHandler.generateResponse(e.getMessage(), HttpStatus.MULTI_STATUS, null);
+        }
     }
 
     @DeleteMapping("/delete/{id}")
-    public String deleteProduct(@PathVariable int id) {
-        return service.deleteProduct(id);
+    public ResponseEntity<Object> deleteProduct(@PathVariable int id) {
+        try {
+            String message = service.deleteProduct(id);
+            return ResponseHandler.generateResponse(message, HttpStatus.OK, null);
+        } catch (Exception e) {
+            return ResponseHandler.generateResponse(e.getMessage(), HttpStatus.MULTI_STATUS, null);
+        }
     }
 
 }
