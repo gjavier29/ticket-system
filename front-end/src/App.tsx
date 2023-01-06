113383f7f57a3363d17c95adcbf49ee912fac9cf
gjavier <gjavier@csyoutsourcing.com>
2023-01-06 14:04:58 +0800

    update ui

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,11 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import React from 'react';
+// import logo from './logo.svg';
+// import './App.css';
+
+import { Home } from './screens'
 
 function App() {
   return (
     <div className="App">
-      <div className="">sample</div>
+      {/* <div className="">sample</div> */}
+      <Home />
     </div>
   );
 }
